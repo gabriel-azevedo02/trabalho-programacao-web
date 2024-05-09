aa44db99c8474e4b28f59ccb08b2914a08b95522
gabriel-ribeiro02 <gabrielribeiroazevedo02@gmail.com>
2024-05-09 09:43:30 -0300

    function checar ganhador

--- scripts.js
+++ scripts.js
@@ -1,8 +1,19 @@
 const celulas = document.querySelectorAll(".celula");
-let checarTurno = true;
+let checarVez = true;
 
 const JOGADOR_X = "X";
 const JOGADOR_O = "O";
+
+const COMBINACOES = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
 
 document.addEventListener("click", (event) => {
     if (event.target.matches(".celula")) {
@@ -13,12 +24,29 @@ document.addEventListener("click", (event) => {
 
 function play(id) {
     const celula = document.getElementById(id);
-    turno = checarTurno ? JOGADOR_X : JOGADOR_O;
-    celula.textContent = turno;
-    checarTurno = !checarTurno;
-    checkWin(turno);
+    vez = checarVez ? JOGADOR_X : JOGADOR_O;
+    celula.textContent = vez;
+    celula.classList.add(vez);
+    checkWin(vez);
 }
 
-function checkWin() {
+function checkWinner(vez) {
 
+    const winner = COMBINACOES.some((comb) => {
+        return comb.every((index) => {
+         return celulas[index].classList.contains(vez);   
+
+        })
+    });
+     
+    if(winner){
+        endGame(vez);
+    } else if(checkTie()){
+        endGame();
+    } else {
+        checarVez = !checarVez;
+    }
+    
+
+   
 }
